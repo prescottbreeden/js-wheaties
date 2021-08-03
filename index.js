f9c8f3dc90a8c15a277ed35790efb68568ca6ca7
prescottbreeden <prescott.breeden@gmail.com>
2021-08-02 23:47:50 -0700

    playing around with immutable ds

--- index.js
+++ index.js
@@ -5,5 +5,29 @@ console.log('running');
 // require('./functional-data-structures/sll');
 // require('./functional-data-structures/bst');
 // require('./cracking/arrays-strings');
-require('./cracking/linked-lists');
+// require('./cracking/linked-lists');
+
+const findMaker = (str, markers) => {
+  for(let i = 0; i < markers.length; i++) {
+    if (str.includes(markers[i])) {
+      return markers[i];
+    }
+  }
+  return undefined;
+}
+
+function checkComments(input, markers) {
+  const res = input.split(/\n/)
+  const balls = res.map((s) => {
+    const marker = findMaker(s, markers);
+    return marker 
+      ? s.split(marker)[0].trim()
+      : s;
+  }).join('\n')
+  return balls;
+}
+const res = checkComments(
+  "apples, plums % and bananas\npears\noranges !applesauce", 
+  ["%", "!"], 
+)
 
