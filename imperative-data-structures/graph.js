9d2a98ff2b5b9058132352177157c7c9c55de0f6
bobross <androgenagonist@gmail.com>
2021-08-08 01:26:00 -0700

    graph updates

--- imperative-data-structures/graph.js
+++ imperative-data-structures/graph.js
@@ -1,43 +1,5 @@
 console.log('--[ Graphs ]------------------');
 
-// vertex: a node
-// edge: connection between nodes
-// weighted / unweighted
-// directed / undirected
-
-// Adjacency Matrix
-// - More space in sparse graphs
-// - Slower to iterate over all edges
-// - Faster to lookup specific edge
-// [- A B C D E F]
-// [A 0 1 0 0 0 1]
-// [B 1 0 1 0 0 0]
-// [C 0 1 0 1 0 0]
-// [D 0 0 1 0 1 0]
-// [E 0 0 0 1 0 1]
-// [F 1 0 0 0 1 0]
-matrix = [
-  [0, 1, 0, 0, 0, 1],
-  [1, 0, 1, 0, 0, 0],
-  [0, 1, 0, 1, 0, 0],
-  [0, 0, 1, 0, 1, 0],
-  [0, 0, 0, 1, 0, 1],
-  [1, 0, 0, 0, 1, 0],
-];
-
-// Adjacency List * better use case in real-world data
-// - Less space in sparse graphs
-// - Faster to iterate over all edges
-// - Slower to lookup specific edge
-list = {
-  A: ['F', 'B'],
-  B: ['A', 'C'],
-  C: ['B', 'D'],
-  D: ['C', 'E'],
-  E: ['D', 'F'],
-  F: ['E', 'A'],
-};
-
 // BIG O
 // |V| - number of vertices
 // |E| - number of edges
@@ -52,47 +14,58 @@ list = {
 // Storate        | O(|V| + |E|)    | O(|V^2|)
 
 class Graph {
+  #list;
   constructor() {
-    this.list = {};
+    this.#list = {};
+  }
+
+  print() {
+    console.log(this.#list);
   }
 
   addVertex(vertex) {
-    if (!this.list[vertex]) {
-      this.list[vertex] = [];
+    if (this.#list[vertex]) {
+      return this;
+    } else {
+      this.#list[vertex] = [];
+      return this;
     }
   }
 
-  addEdge(v1, v2, weight) {
-    this.list[v1] = [...this.list[v1], { node: v2, weight }];
+  addEdge(v1, v2) {
+    this.#list[v1] = [...this.#list[v1], v2];
+    return this;
   }
 
   removeEdge(v1, v2) {
-    this.list[v1] = this.list[v1].filter((v) => v.node !== v2);
+    this.#list[v1] = this.#list[v1].filter((v) => v !== v2);
+    return this;
   }
 
   removeVertex(vertex) {
-    this.list = Object.keys(this.list).reduce((acc, curr) => {
+    this.#list = Object.keys(this.#list).reduce((acc, curr) => {
       return curr == vertex
         ? acc
         : {
             ...acc,
-            [curr]: this.list[curr].filter((v) => v.node !== vertex),
+            [curr]: this.#list[curr].filter((v) => v !== vertex),
           };
     }, {});
+    return this;
   }
 
   dfs(start) {
-    if (!this.list[start]) return null;
+    if (!this.#list[start]) return [];
     const result = [];
     const visited = { [start]: true };
-    const list = this.list;
+    const list = this.#list;
 
     (function traverse(v) {
       list[v].forEach((n) => {
-        if (!visited[n.node]) {
+        if (!visited[n]) {
           result.push(n);
-          visited[n.node] = true;
-          traverse(n.node);
+          visited[n] = true;
+          traverse(n);
         }
       });
     })(start);
@@ -100,56 +73,49 @@ class Graph {
     return result;
   }
 
-  printMe() {
-    Object.keys(this.list).forEach((r) => {
-      console.log('---------------');
-      console.log(r, ':', this.list[r]);
-    });
+  depthFirst(start) {
+    const result = [];
+    const visited = { [start]: true };
+    const stack = [start];
+    while (stack.length > 0) {
+      const node = stack.pop();
+      this.#list[node].forEach((n) => {
+        if (!visited[n]) {
+          visited[n] = true;
+          stack.push(n);
+          result.push(n);
+        }
+      });
+    }
+    return result;
+  }
+
+  bfs(start) {
+    // implement a queue
+    return this;
+  }
+
+  dijkstras() {
+    // implement a priority queue
+    return this;
   }
 }
 
-const bob = new Graph();
-const seattle = 'Seattle';
-const jfk = 'JFK';
-const anchorage = 'Anchorage';
-const frankfurt = 'Frankfurt';
-const iceland = 'Iceland';
-const ireland = 'Ireland';
-const moscow = 'Moscow';
+const bob = ['A', 'B', 'C', 'D', 'E', 'F'].reduce((acc, curr) => {
+  return acc.addVertex(curr);
+}, new Graph());
 
-bob.addVertex(seattle);
-bob.addVertex(jfk);
-bob.addVertex(anchorage);
-bob.addVertex(frankfurt);
-bob.addVertex(iceland);
-bob.addVertex(ireland);
-bob.addVertex(moscow);
+bob.addEdge('A', 'B');
+bob.addEdge('B', 'A');
+bob.addEdge('C', 'E');
+bob.addEdge('D', 'C');
+bob.addEdge('E', 'D');
+bob.addEdge('E', 'F');
+bob.addEdge('F', 'C');
+bob.addEdge('F', 'A');
 
-bob.addEdge(moscow, seattle, 10.5);
-
-bob.addEdge(seattle, jfk, 5);
-bob.addEdge(seattle, frankfurt, 10);
-bob.addEdge(seattle, anchorage, 3);
-
-bob.addEdge(anchorage, seattle, 3);
-bob.addEdge(anchorage, moscow, 9);
-
-bob.addEdge(jfk, seattle, 5);
-bob.addEdge(jfk, frankfurt, 8);
-bob.addEdge(jfk, iceland, 4);
-bob.addEdge(jfk, anchorage, 7);
-
-bob.addEdge(iceland, jfk, 5.5);
-bob.addEdge(iceland, ireland, 2);
-
-bob.addEdge(ireland, iceland, 2);
-bob.addEdge(ireland, frankfurt, 1.5);
-
-bob.addEdge(frankfurt, ireland, 1.5);
-bob.addEdge(frankfurt, jfk, 8);
-bob.addEdge(frankfurt, anchorage, 9.5);
-
-// bob.removeEdge(frankfurt, seattle);
-bob.printMe();
-
-console.log('DFS: ', bob.dfs(seattle));
+bob.print();
+const dfs = bob.dfs('F');
+console.log(dfs);
+const dfs2 = bob.depthFirst('F');
+console.log(dfs2);
