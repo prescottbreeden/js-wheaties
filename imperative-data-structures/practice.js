004648b57359294c7559f0ad0858b3a83fd04519
bobross <androgenagonist@gmail.com>
2021-08-14 01:35:34 -0700

    renamed and added commented bullets for algos

---
+++ imperative-data-structures/practice.js
@@ -0,0 +1,74 @@
+// ---------------------
+// Concepts / Strategies
+// ---------------------
+// Dynamic Programming
+// Divide and Conquer
+// Moving Window
+// Multiple Pointer
+// Array Partition
+
+// ----------------
+// Sorts
+// ----------------
+// Bubble
+// Selection
+// Insertion
+// Merge
+// Quick
+// Radix
+
+// ----------------
+// Data Structures
+// ----------------
+// SLL
+// addfront
+// addback
+// removefront
+// removeback
+
+// STACK
+// push
+// pop
+// peek
+
+// QUEUE (reg/circuluar)
+// enqueue
+// dequeue
+
+// HEAP (max/min/priority)
+// insert
+// extract root
+
+// BST
+// insert
+// delete
+// contains
+// filter
+// preorder
+// inorder
+// postorder
+// bfs
+// mintree
+// list of depths
+// check balanced
+// valid bst
+// successor
+
+// Red-Black Tree
+// maybe?
+
+// GRAPH (un/directed, un/weighted)
+// addvertex
+// addedge
+// bfs
+// dfs
+// shortestpath
+// dijkstra
+
+// TRIE
+// insert
+// remove
+// suggestions
+
+// Special
+// A*
