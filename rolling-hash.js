2e1de28ce641db4091a55cd366c754e6ebf14e45
bobross <androgenagonist@gmail.com>
2021-08-17 18:33:39 -0700

    rolling hash

--- rolling-hash.js
+++ rolling-hash.js
@@ -1,34 +1,45 @@
+const pipe = (...fns) => (arg) => fns.reduce((acc, curr) => curr(acc), arg);
+const add = (a) => (b) => a + b;
+const subtractFrom = (a) => (b) => a - b;
+const subtractBy = (a) => (b) => b - a;
+const mod = (a) => (b) => b % a;
+const pow2 = (a) => (b) => b ** a;
+const pow = (a) => (b) => a ** b;
+const mult = (a) => (b) => a * b;
+const hash = (a) => a.charCodeAt(0);
+
+const positionValue = (p) => (char) => pipe(hash, mult(pow(10)(p)))(char);
+
+const stringSum = (arr) =>
+  arr.reduce(
+    (acc, curr, index) =>
+      pipe(positionValue(arr.length - index - 1), add(acc))(curr),
+    0
+  );
+
+const h = (length) => (slide) => (end) => (start) =>
+  pipe(
+    positionValue(length),
+    subtractFrom(slide),
+    mult(10),
+    add(hash(end))
+  )(start);
+
 const rollingHash = (pattern, text) => {
-  const table = text.split('').reduce((acc, curr) => {
-    return { ...acc, [curr]: curr.charCodeAt(0) };
-  }, {});
-  const stringSum = (arr) =>
-    arr.reduce(
-      (acc, curr, index) =>
-        acc + ((table[curr] * 10 ** (arr.length - index - 1)) % 113),
-      0
-    );
   const result = stringSum(pattern.split(''));
   let start = 0;
-  let end = pattern.length - 1;
-  let slide = stringSum(['t', 'h', 'e']);
-  while (end < text.length) {
-    console.log('---------------');
-    console.log(text.slice(start, end + 1));
-    console.log('slide/result', slide, result);
-    // if (slide === result) {
-    if (stringSum(text.slice(start, end + 1).split('')) === result) {
-      return true;
-    }
-    slide -= (table[text[start]] * 10 ** (pattern.length - 1)) % 113;
+  let end = pattern.length - 1; // 2
+  let slide = stringSum(text.slice(start, end + 1).split(''));
+
+  while (end < text.length - 1) {
+    if (slide === result) return true;
+    slide = h(pattern.length - 1)(slide)(text[end + 1])(text[start]);
     start++;
     end++;
-    slide += (table[text[end]] * 10 ** 0) % 113;
   }
   return false;
 };
 
 const text = 'thebrownfoxjumpsoverthelazydog';
-const pattern = 'fox';
-
+const pattern = 'foxj';
 console.log(rollingHash(pattern, text));
